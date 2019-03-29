da53d2b981c5c6ce639a218ff6a9372a2b1a9a96
Apollo3zehn <vwilms@outlook.com>
2019-03-29 11:31:08 +0100

    v9

--- test.ps1
+++ test.ps1
@@ -1,33 +1,24 @@
-if ($env:APPVEYOR_JOB_NAME -eq $env:JobToWait)
+function Start-ArtifactDownload([string]$jobName, [string]$fileName)
 {
-    Get-Date > date.txt  
-    Push-AppveyorArtifact ./date.txt
-}
-
-if ($env:APPVEYOR_JOB_NAME -ne $env:JobToWait) 
-{
-    write-host "Waiting for job `"$env:JobToWait`" to complete"
-    
     $headers = @{
         "Authorization" = "Bearer $ApiKey"
         "Content-type" = "application/json"
     }
 
-    $stop = ([DateTime]::Now).AddMinutes($env:TimeOutMins)
-    $success = $false  
-    
-    while(!$success -and ([DateTime]::Now) -lt $stop) 
+    $project = Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG" -Headers $headers -Method GET
+    $job = $project.build.jobs | Where-Object {$_.name -eq $jobName}  
+    $success = $job.status -eq "success"
+    $jobId = $job.jobId;
+
+    if (!$success) 
     {
-        $project = Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG" -Headers $headers -Method GET
-    
-        $JobToWaitJson = $project.build.jobs | where {$_.name -eq $env:JobToWait}  
-        $success = $JobToWaitJson.status -eq "success"
-        $JobToWaitId = $JobToWaitJson.jobId;
-        if (!$success) {Start-sleep 5}
+        throw "Job `"$jobName`" was not finished successfully"
     }
-    
-    if (!$success) {throw "Job `"$env:JobToWait`" was not finished in $env:TimeOutMins minutes"}
-    if (!$JobToWaitId) {throw "Unable to get JobId for the job `"$env:JobToWait`""}
-    
-    Start-FileDownload  https://ci.appveyor.com/api/buildjobs/$JobToWaitId/artifacts/date.txt
+
+    if (!$jobId) 
+    {
+        throw "Unable to get job ID for the job `"$jobName`""
+    }
+
+    Start-FileDownload https://ci.appveyor.com/api/buildjobs/$jobId/artifacts/$fileName.txt
 }
